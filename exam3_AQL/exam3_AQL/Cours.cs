dbd422c065fc18cd72fdb014be454c6cf3de8f7e
karima <dr.kebir.karima@hotmail.fr>
2021-04-12 08:18:50 -0400

    Ajout de la charpente du projet

---
+++ exam3_AQL/exam3_AQL/Cours.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exam3_AQL
+{
+    public class Cours
+    {
+        public int NumeroCours { get; set; }
+        public string CodeCours { get; set; }
+        public string TitreCours { get; set; }
+
+
+        //Constructeur
+        public Cours(int numeroCours, string codeCours, string titreCours)
+        {
+            this.NumeroCours = numeroCours;
+            this.CodeCours = codeCours;
+            this.TitreCours = titreCours;
+        }
+    }
+}
---
+++ exam3_AQL/exam3_AQL/Etudiant.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exam3_AQL
+{
+    public class Etudiant
+    {
+        public int NumeroEtudiant { get; set; }
+        public string Nom { get; set; }
+        public string Prenom { get; set; }
+        public List<Cours> ListeDeCours { get; set; }
+
+
+        //Constructeur
+        public Etudiant(int numeroEtudiant, string nom, string prenom)
+        {
+            this.NumeroEtudiant = numeroEtudiant;
+            this.Nom = nom;
+            this.Prenom = prenom;
+            this.ListeDeCours = new List<Cours>();
+        }
+    }
+}
