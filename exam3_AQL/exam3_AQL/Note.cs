81932ae1143f8fa2d09013f2f122c8eb63547cf5
Mouna Tebourski <mouna.tebourski.pro@gmail.com>
2021-04-13 16:20:37 -0400

    commit pour classe Note

--- exam3_AQL/exam3_AQL/Note.cs
+++ exam3_AQL/exam3_AQL/Note.cs
@@ -9,26 +9,16 @@ namespace Exam3_AQL
     public class Note
     {
         public int NumeroEtudiant { get; set; }
-<<<<<<< HEAD
-        public int CodeCours { get; set; }
-        public double NoteCours { get; set; }
-
-        public Note(int numeroEtudiant, int codeCours, double noteCours)
-=======
         public string CodeCours { get; set; }
         public double NoteCours { get; set; }
 
+       
         public Note(int numeroEtudiant, string codeCours, double noteCours)
->>>>>>> refs/remotes/origin/main
         {
             this.NumeroEtudiant = numeroEtudiant;
             this.CodeCours = codeCours;
             this.NoteCours = noteCours;
         }
-<<<<<<< HEAD
 
-
-=======
->>>>>>> refs/remotes/origin/main
     }
 }
