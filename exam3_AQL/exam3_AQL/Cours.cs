9333e242f58c29f317eceba7ddfd9df3d79ec89e
Laila17 <2691473@collegelacite.ca>
2021-04-13 00:47:36 -0400

    Ajout des propriétés et constrcuteur ainsi que la méthode Tosring à la classe cours

--- exam3_AQL/exam3_AQL/Cours.cs
+++ exam3_AQL/exam3_AQL/Cours.cs
@@ -6,8 +6,57 @@ using System.Threading.Tasks;
 
 namespace Exam3_AQL
 {
-    class Cours
+   public class Cours
+
     {
-        //test
+        /// <summary>
+        /// Proprités de la classe cours
+        /// </summary>
+
+        public int NumeroCours { get; set; }
+        public string CodeCours { get; set; }
+        public string TitreCours { get; set; }
+
+
+        /// <summary>
+        /// Constructeur initialisant tous les attributs de la classe cours
+        /// </summary>
+        public Cours(int numeroCours, string codeCours, string titreCours)
+        {
+            this.NumeroCours = numeroCours;
+            this.CodeCours = codeCours;
+            this.TitreCours = titreCours;
+        }
+
+
+        /// <summary>
+        /// Méthode ToString permetant de convertir l'objet cours vers une chaîne de caractère
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return base.ToString() + "\n" +
+                 "Numéro de cours : " + this.NumeroCours + "\n" +
+                 "Code : " + this.CodeCours + "\n" +
+                 "Titre : " + this.TitreCours;
+        }
+
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
