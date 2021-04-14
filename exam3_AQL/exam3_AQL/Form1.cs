e52240edeba7348100b3d4679252b8801ffda0c5
MounaTebourski <82182296+MounaTebourski@users.noreply.github.com>
2021-04-14 02:34:25 -0400

    Update Form1.cs
    fixe conflits

--- exam3_AQL/exam3_AQL/Form1.cs
+++ exam3_AQL/exam3_AQL/Form1.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,38 +12,30 @@ namespace Exam3_AQL
 {
     public partial class Form1 : Form
     {
-<<<<<<< HEAD
-        
 
+        private int incrementCours = 1;
+        public List<Cours> CoursList = new List<Cours>();
         public List<Etudiant> ListeEtudiants = new List<Etudiant>();
+		
 
         public Form1()
         {
             InitializeComponent();
-            textNumEtudiant.Text = 1 +"";
-=======
-        List<Cours> CoursList = new List<Cours>();
-        int incrementCours = 1;
-        public Form1()
-        {
-            InitializeComponent();
-           
->>>>>>> 1db5be9746f086706f695a04d3b1e0fcbe034ac8
+            
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
             textNumCours.Text = incrementCours.ToString();
+			textNumEtudiant.Text = 1 +"";
 
         }
 
         //Bouton Ajouter cours
-        private void button1_Click(object sender, EventArgs e)
+        private void buttonAjoutCours_Click(object sender, EventArgs e)
         {
-<<<<<<< HEAD
-           
-=======
-            try
+
+           try
             {
 
                 Cours cours = new Cours(int.Parse(textNumCours.Text), textCodeCours.Text, textTitreCours.Text);
@@ -51,7 +43,6 @@ namespace Exam3_AQL
                 dataGridView.Rows.Add(cours.NumeroCours, cours.CodeCours, cours.TitreCours);
                 CoursList.Add(cours);
                 incrementCours++;
-
 
                 textNumCours.Text = incrementCours.ToString();
                 textCodeCours.Clear();
@@ -62,14 +53,10 @@ namespace Exam3_AQL
             {
                 MessageBox.Show("Vous devez saisir un nombre dans le champs : Numéro de cours");
             }
->>>>>>> 1db5be9746f086706f695a04d3b1e0fcbe034ac8
-        }
-
-        private void textNumCours_TextChanged(object sender, EventArgs e)
-        {
 
         }
 
+  
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
 
@@ -77,6 +64,8 @@ namespace Exam3_AQL
 
         private void buttonAjoutEtudiant_Click(object sender, EventArgs e)
         {
+            try
+            {
             Etudiant etudiant = new Etudiant (textNomEtudiant.Text, textPrenomEtudiant.Text);
 
             textNumEtudiant.Text = etudiant.NumeroEtudiant + "";
@@ -92,26 +81,17 @@ namespace Exam3_AQL
             textNomEtudiant.Clear();
             textPrenomEtudiant.Clear();
             textNumEtudiant.Text = increment + "";
+
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Vous devez saisir les types convenables");
+            }
         } 
 
-        private void groupBox1_Enter(object sender, EventArgs e)
-        {
+      
+     
 
-        }
 
-        private void groupBox2_Enter(object sender, EventArgs e)
-        {
-
-        }
-
-        private void textNumEtudiant_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void textNumEtudiant_Click(object sender, EventArgs e)
-        {
-
-        }
     }
 }
